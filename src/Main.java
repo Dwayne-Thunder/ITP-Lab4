95d0496b72af556dbd170640d58d1f3b92a87368
Dwayne Thunder <mohovikov.denis@gmail.com>
2024-10-23 14:29:57 +0300

    1 Задание

--- src/Main.java
+++ src/Main.java
@@ -4,20 +4,21 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         //Зона переменных
-        int x;//Вводимый делитель
-        int y;//Будущее значение функции
         int m;//Переменная для выбора функции меню
 
 
-        // Создание объекта Scanner для чтения данных с клавиатуры
-        Scanner scanner = new Scanner(System.in);
         // Создаем BufferedReader для чтения с консоли
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         // Создаем PrintWriter для вывода на консоль
         PrintWriter writer = new PrintWriter(System.out, true);
+        // Создание объекта Scanner для чтения данных с клавиатуры
+        Scanner scanner = new Scanner(System.in);
 
 
+        writer.println("Выберите функцию: \nДопустимые номера: \n1 - 1 задание\n2 - 2 задание \n3 - 3 задание \n4 для выхода");
         m = scanner.nextInt();// Ввод М с проверкой на целочисленность
+        scanner.close();
+        Menu(m);
 
 
 
@@ -26,18 +27,6 @@ public class Main {
 
 
 
-        try {
-            writer.println("Введите значение X:");
-            x = scanner.nextInt();
-
-            y=285/x;
-            writer.println("Результат функции  y = 285 / "+ x+" равен:"+ y);
-            // Закрытие Scanner
-            scanner.close();
-        } catch (ArithmeticException zero) {
-        // перехватить исключение
-            System.out.println("Ты ШАЙТАН делишь на ноль!");
-        }
 
 
     }
@@ -45,11 +34,37 @@ public class Main {
     public static void Menu(int funk) {
         switch (funk) {
             case '1':
+                //Зона переменных
+                int x;//Вводимый делитель
+                int y;//Будущее значение функции
+
+                // Создаем BufferedReader для чтения с консоли
+                BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+                // Создаем PrintWriter для вывода на консоль
+                PrintWriter writer = new PrintWriter(System.out, true);
+                // Создание объекта Scanner для чтения данных с клавиатуры
+                Scanner scanner = new Scanner(System.in);
+
+
+                try {
+                    writer.println("Введите значение X:");
+                    x = scanner.nextInt();
+
+                    y = 285 / x;
+                    writer.println("Результат функции  y = 285 / " + x + " равен:" + y);
+                    // Закрытие Scanner
+                    scanner.close();
+                } catch (ArithmeticException zero) {
+                    // перехватить исключение
+                    System.out.println("Ты ШАЙТАН делишь на ноль!");
+                }
+
             case '2':
             case '3':
-            case 'X':
+            case '4':
+
             default:
-                System.out.println("Неправильный номер! \nДопустимые номера: \n1 - 1 задание\n2 - 2 задание \n3 - 3 задание \nX для выхода\"");
+                System.out.println("Неправильный номер! \nДопустимые номера: \n1 - 1 задание\n2 - 2 задание \n3 - 3 задание \n4 для выхода");
 
         }
     }
