202e240f2b517c051af1c8ed6d7ebd87d96ff5be
Lintuq <91467814+Lintuq@users.noreply.github.com>
2024-11-07 21:34:58 +0300

    доделанная работа

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,25 @@
 import java.io.*;
 
+class LetterOutRange extends Exception{
+    public LetterOutRange(String mes){ //КОнструктор с сообщением об ошибке
+        super(mes); // Обращение к родительскому классу
+    }
+}
+
 public class Main {
     public static void main(String[] args) throws IOException {
-        //Зона переменных
-        int m;//Переменная для выбора функции меню
 
+        int m;//Переменная для выбора функции меню
 
         // Создаем BufferedReader для чтения с консоли
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         // Создаем PrintWriter для вывода на консоль
         PrintWriter writer = new PrintWriter(System.out, true);
 
-
-
         writer.println("Выберите функцию: \nДопустимые номера: \n1 - 1 задание\n2 - 2 задание \n3 - 3 задание \n4 для выхода");
         m = reader.read();// Ввод М с проверкой на целочисленность
         Menu(m);
-
-
     }
-
 
     // Метод для заполнения символьного массива
     public static void enterArray(char[] array) throws IOException, ArrayIndexOutOfBoundsException {
@@ -31,22 +31,38 @@ public class Main {
         }
     }
 
+    public static char shiftLetterChar(char letter){
+        PrintWriter writer = new PrintWriter(System.out, true);
+        int shift_char_code;
 
+        if (Character.isUpperCase(letter)) {
+            // Сдвиг заглавной буквы на 2 разряда влево
+            shift_char_code = letter << 2;
+            //writer.println("Сдвиг заглавной буквы на 2 разряда влево. Десятичное значение: " + shift_char_code);
+        } else if (Character.isLowerCase(letter)) {
+            // Сдвиг строчной буквы на 1 разряд вправо
+            shift_char_code = letter >> 1;
+            //writer.println("Сдвиг строчной буквы на 1 разряд вправо. Десятичное значение: " + shift_char_code);
+        } else {
+            // Если символ не является буквой, возвращаем его без изменений
+            writer.println("Символ не является заглавной или строчной буквой. " + letter);
+            return letter;
+        }
 
-
+        return (char) shift_char_code;
+    }
 
     public static void Menu(int funk) {
+        // Создаем BufferedReader для чтения с консоли
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        // Создаем PrintWriter для вывода на консоль
+        PrintWriter writer = new PrintWriter(System.out, true);
+
         switch (funk) {
             case '1':
                 //Зона переменных
                 int x;//Вводимый делитель
                 int y;//Будущее значение функции
-
-                // Создаем BufferedReader для чтения с консоли
-                BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-                // Создаем PrintWriter для вывода на консоль
-                PrintWriter writer = new PrintWriter(System.out, true);
-
 
                 try {
                     writer.println("Введите значение X:");
@@ -57,10 +73,13 @@ public class Main {
 
                 } catch (ArithmeticException zero) {
                     // перехватить исключение
-                    System.out.println("Ты ШАЙТАН делишь на ноль!");
+                    writer.println("Ты ШАЙТАН делишь на ноль!");
                 } catch (IOException e) {
-                    System.out.println("Ошибка ввода: " + e.getMessage());
+                    writer.println("Ошибка ввода: " + e.getMessage());
+                } catch (NumberFormatException nfe){
+                    writer.println("Ошибка: введено не число. Пожалуйста, введите целое число.");
                 }
+                break;
 
             case '2':
                 // Создаем символьный массив
@@ -70,22 +89,57 @@ public class Main {
                     enterArray(charArray);
                 } catch (ArrayIndexOutOfBoundsException e) {
                     // Обрабатываем исключение выхода за границы массива
-                    System.out.println("Ошибка: Выход за пределы массива! Позиция: " + e.getMessage());
+                    writer.println("Ошибка: Выход за пределы массива! Позиция: " + e.getMessage());
                 } catch (IOException e) {
                     // Обрабатываем исключение ввода-вывода
-                    System.out.println("Ошибка ввода: " + e.getMessage());
+                    writer.println("Ошибка ввода: " + e.getMessage());
                 }
 
                 // Вывод массива
-                System.out.println("Массив символов: ");
+                writer.println("Массив символов: ");
                 for (char c : charArray) {//Для каждого элемента c в массиве charArray выполните следующее
-                    System.out.print(c + " ");//выводит текущий символ (c) с добавлением пробела после него
+                    writer.print(c + " ");//выводит текущий символ (c) с добавлением пробела после него
                 }
+                writer.flush();
+                break;
             case '3':
+
+                char upperLetter = 'J';  // Заглавная буква
+                char lowerLetter = 'c';  // Строчная буква
+
+                char shiftedUpper = shiftLetterChar(upperLetter);
+                char shiftedLower = shiftLetterChar(lowerLetter);
+
+                writer.println("Заданная заглавная буква: " + upperLetter + " Код заглавной буквы: " + (int) upperLetter);
+                writer.println("Преобразованная заглавная буква: " + shiftedUpper + " Код преобразованной буквы: " + (int) shiftedUpper);
+
+                writer.println(" ");
+
+                writer.println("Заданная строчная буква: " + lowerLetter + " Код строчной буквы: " + (int) lowerLetter);
+                writer.println("Преобразованная строчная буква: " + shiftedLower + " Код преобразованной буквы: " + (int) shiftedLower);
+
+                //Проверка диапазона
+                try
+                {
+                    boolean upperOORan = (int)shiftedUpper < 32 || (int)shiftedUpper > 175;
+                    boolean lowerOORan = (int)shiftedLower < 32 || (int)shiftedLower > 175;
+
+                    if (upperOORan && lowerOORan) {
+                        throw new LetterOutRange("Обе буквы вне диапазона");
+                    } else if (upperOORan) {
+                        throw new LetterOutRange("Первая буква вне диапазона");
+                    } else if (lowerOORan) {
+                        throw new LetterOutRange("Вторая буква вне диапазона");
+                    }
+                } catch (LetterOutRange e) {
+                    writer.println("Ошибка: " + e.getMessage());
+                }
+
+                break;
             case '4':
                 break;
             default:
-                System.out.println("Неправильный номер! \nДопустимые номера: \n1 - 1 задание\n2 - 2 задание \n3 - 3 задание \n4 для выхода");
+                writer.println("Неправильный номер! \nДопустимые номера: \n1 - 1 задание\n2 - 2 задание \n3 - 3 задание \n4 для выхода");
 
         }
     }
